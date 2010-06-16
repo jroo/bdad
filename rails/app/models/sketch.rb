99be2c90f6db7eccb1296d9d783a691b5d5f1da1
David James <djames@sunlightfoundation.com>
2010-06-16 11:18:33 -0400

    cruft removal

--- rails/app/models/sketch.rb
+++ rails/app/models/sketch.rb
@@ -9,19 +9,4 @@ class Sketch < ActiveRecord::Base
     screen_data = ScreenData.find_by_token(self.token)
     self.screen_data, self.map_data = screen_data.value, screen_data.map_data
   end
-  
-  # def user_name
-  #   user.name
-  # end
-  # 
-  # def user_name=(value)
-  #   user ||= User.find(self.user_id)
-  #   if user
-  #     # ...
-  #   else
-  #     user = User.find(self.user_id)
-  #   end
-  #   user.name = value
-  #   user.save!
-  # end
 end
