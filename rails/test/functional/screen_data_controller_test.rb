66f0090405b4d4c7293c9b3e0472b9a94829e8dd
David James <djames@sunlightfoundation.com>
2010-06-16 11:20:12 -0400

    be sure to pass in a string to 'value'
    if you pass in a hash, then Rails will magically (and
    mysteriously) tranform it to YAML, which gets stored
    in the database. At that point, it is just a string and
    we won't know if it is JSON or YAML

--- rails/test/functional/screen_data_controller_test.rb
+++ rails/test/functional/screen_data_controller_test.rb
@@ -2,26 +2,32 @@ require 'test_helper'
 
 class ScreenDataControllerTest < ActionController::TestCase
   test "can get existing screen data" do
-    data = ScreenData.create({:value => '{map:{paths: [M123 456 L 234 345z]}}', :token => 'ABCDE'})
+    data = ScreenData.create({
+      :value => {'paths' => ['M123 456 L 234 345z']}.to_json,
+      :token => 'ABCDE'})
     get :index, :t => 'ABCDE'
     assert_response :success
   end
   
   test "can set screen data for an existing token" do
-    data = ScreenData.create({:value => '{map:{paths: [M789 52 L 687 670]}}', :token => 'ABCDE'})
-    post :create, :t => 'ABCDE', :d => '{map:{paths: [M123 456 L 234 345z]}}'
+    data = ScreenData.create({
+      :value => {'paths' => ['M789 52 L 687 670z']}.to_json,
+      :token => 'ABCDE'
+    })
+    post :create, :t => 'ABCDE', :d => '{paths: [M123 456 L 234 345z]}'
     assert_response :success
     data.reload
-    assert_equal '{map:{paths: [M123 456 L 234 345z]}}', data.value
+    assert_equal %({paths: [M123 456 L 234 345z]}), data.value
   end
   
   test "can set screen data for a new token" do
-    post :create, :t => 'ABCDE', :d => '{map:{paths: [M123 456 L 234 345z]}}'
+    post :create, :t => 'ABCDE',
+      :d => {'paths'=> ['M123 456 L 234 345z']}.to_json
     assert_response :success
     data = ScreenData.find_by_token('ABCDE')
-    assert_equal '{map:{paths: [M123 456 L 234 345z]}}', data.value
+    assert_equal({'paths' => ['M123 456 L 234 345z']}.to_json, data.value)
   end
-  
+
   test "trying to get screen data for a missing sketch results in 404" do
     get :index, :t => 'DEFGH'
     assert_response :missing
--- rails/test/unit/sketch_test.rb
+++ rails/test/unit/sketch_test.rb
@@ -3,14 +3,14 @@ require 'test_helper'
 class SketchTest < ActiveSupport::TestCase
   test 'finds screen_data on save and copies paths' do
     sketch = Sketch.new({:token => 'ABCDE', :title => 'Test Title'})
-    data = ScreenData.create({
+    data = ScreenData.create!({
       :token => 'ABCDE', 
-      :value => '{map: {paths: [M 123 456 L 234 567z]}}'
+      :value => {'paths' => ['M 123 456 L 234 567z']}.to_json
     })
     sketch.save
     sketch.reload
-    assert_equal '{map: {paths: [M 123 456 L 234 567z]}}', sketch.screen_data
-    assert_equal MultiPolygon.from_coordinates([[[123,456], [234,567]]]), sketch.map_data
+    assert_equal %({"paths":["M 123 456 L 234 567z"]}), sketch.screen_data
+    assert sketch.map_data.is_a?(MultiPolygon)
   end
 
 end
