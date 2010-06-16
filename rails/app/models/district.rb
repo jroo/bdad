0944bc31d10fa2f2323b0b819acaf67aa37ff02e
David James <djames@sunlightfoundation.com>
2010-06-16 12:34:15 -0400

    Add support for district_code

--- rails/app/models/district.rb
+++ rails/app/models/district.rb
@@ -1,6 +1,24 @@
+# create_table "districts", :force => true do |t|
+#   t.string   "state"
+#   t.string   "number"
+#   t.datetime "created_at"
+#   t.datetime "updated_at"
+# end
+
 class District < ActiveRecord::Base
   
+  # state is a two letter code
+  
   has_many :sketches
+  
+  # Two digits for state + Two digits for district
+  # For example, "3301":
+  # 33 = New Hampshire
+  # 01 = 1st District
+  # (from http://www.cs.princeton.edu/introcs/data/codes.csv)
+  def code
+    "%02i%02i" % [FIPS_CODES[state], number]
+  end
   
   def self.find_random
     district = District.first
