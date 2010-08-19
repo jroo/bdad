8b67651cef4c09159bab8470bb48601391b89ac7
David James <davidcjames@gmail.com>
2010-08-19 09:58:57 -0400

    Update cap deploy settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,24 +10,20 @@ set :user, "bdad"
 set :use_sudo, false
 set :deploy_via, :remote_cache
 set :deploy_to, "/projects/bdad/src/bdad"
-set :domain, "ec2-184-73-118-224.compute-1.amazonaws.com"
-# set :domain, "MetroCenter"
-set :domain, "BdadMetroCenter"
+set :domain, "BdadMetroCenter" # see .ssh/config
 set :branch, 'production'
 
 role :web, domain
 role :app, domain
 role :db,  domain
-# role :db, "morgan.sunlightlabs.org", :primary => true
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
-
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
